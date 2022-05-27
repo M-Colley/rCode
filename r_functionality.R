f0403534dab9e1fac2506257c367f5978354b2a6
Mark Colley <39366189+M-Colley@users.noreply.github.com>
2022-05-27 13:30:42 +0200

    Update r_functionality.R

--- r_functionality.R
+++ r_functionality.R
@@ -175,7 +175,7 @@ checkPackageVersions <- function() {
   } else {
     print("update effectsize!")
   }
-  if (packageVersion("ggstatsplot") >= "0.9.2") {
+  if (packageVersion("ggstatsplot") >= "0.9.3") {
     print("ggstatsplot OK!")
   } else {
     print("update ggstatsplot!")
