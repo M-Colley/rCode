598ceb694b89da3b7c9d0ad52ac20fbc0f795e16
Mark Colley <39366189+M-Colley@users.noreply.github.com>
2023-02-24 14:17:37 -0500

    Update r_functionality.R

--- r_functionality.R
+++ r_functionality.R
@@ -1322,14 +1322,14 @@ reportggstatsplot <- function(p, iv = "independent", dv = "Testdependentvariable
 
   # Create String
   if (stats$method %in% c("Kruskal-Wallis rank sum test", "Friedman rank sum test")) {
-    resultString <- paste0("(\\chisq(", stats$df.error, ")=", statistic, ", \\p{", pValue, "}, r=", effectSize)
+    resultString <- paste0("(\\chisq(", stats$df.error, ")=", statistic, ", \\p{", pValue, "}, r=", effectSize, ")")
   } else if (stats$method %in% c("Paired t-test")) {
-    resultString <- paste0("(t(", stats$df.error, ")=", statistic, ", \\p{", pValue, "}, r=", effectSize)
+    resultString <- paste0("(t(", stats$df.error, ")=", statistic, ", \\p{", pValue, "}, r=", effectSize, ")")
   } else if (stats$method %in% c("Wilcoxon signed rank test")) {
-    resultString <- paste0("(V=", statistic, ", \\p{", pValue, "}, r=", effectSize)
+    resultString <- paste0("(V=", statistic, ", \\p{", pValue, "}, r=", effectSize, ")")
   } else {
     # example: \F{7}{24.62}{1.01}, \p{0.45}
-    resultString <- paste0("(\\F{", stats$df, "}{", stats$df.error, "}{", statistic, "}, \\p{", pValue, "}, r=", effectSize)
+    resultString <- paste0("(\\F{", stats$df, "}{", stats$df.error, "}{", statistic, "}, \\p{", pValue, "}, r=", effectSize, ")")
   }
 
 
